ec4d424f9856136a788a324976b1dc8186a9c212
Jake Wharton <jakewharton@gmail.com>
2011-03-28 23:24:51 -0400

    Add package-level javadoc.

---
+++ src/main/java/com/jakewharton/pingdom/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes which assist in marshalling the native builder methods to proper
+ * remote API calls and the resulting JSON to POJOs specifically for the
+ * Pingdom RESTful API.
+ */
+package com.jakewharton.pingdom;
